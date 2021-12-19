f6c52e558dde4b5b42cde471e839d37012cd6e34
haroonbajwa <haroonbajwa99@gmail.com>
2021-12-19 13:11:09 +0500

    pizza description modal

--- client/src/components/Pizza.jsx
+++ client/src/components/Pizza.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
+import { Modal, Button } from 'react-bootstrap';
 
 const Pizza = ({ pizza }) => {
 
     const [quantity, setQuantity] = useState(1);
     const [variant, setVariant] = useState('small');
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div className="m-5 shadow-lg p-3 mb-5 bg-white rounded">
-            <h1>{pizza.name}</h1>
-            <img src={pizza.image} className="img-fluid" style={{width:'200px',height:'200px'}} />
+            <div onClick={handleShow} style={{cursor:'pointer'}}>
+                <h1>{pizza.name}</h1>
+                <img src={pizza.image} alt="pizza" className="img-fluid" style={{width:'200px',height:'200px'}} />
+            </div>
 
             <div className="flex-container mt-3">
                 <div className="w-100 m-1">
@@ -36,6 +44,31 @@ const Pizza = ({ pizza }) => {
                 <div className="m-1 w-100">
                     <button className="btn btn-danger">ADD TO CART</button>
                 </div>
+            </div>
+
+            <div>
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                    <Modal.Title>{pizza.name}</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                    <img src={pizza.image} alt="pizza" className="img-fluid rounded mx-auto d-block" style={{height:'300px'}} />
+                    <h5 className="mt-3">Description</h5>
+                    <p>{pizza.description}</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Close
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </div>
     )
